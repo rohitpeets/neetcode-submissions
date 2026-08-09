ebdc2a0dea118bdc35bb75db00f1062bfc71a96f
rohitpreet singh <276686195+rohitpeets@users.noreply.github.com>
2026-08-09 01:23:04 -0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean matches(int[] a,int[] b){
+        for(int i=0;i<26;i++){
+            if(a[i]!=b[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int window=s1.length();
+        int[] bank=new int[26];
+        for (char c:s1.toCharArray()){
+            bank[c-'a']++;
+        }
+        int right=s1.length()-1;;
+        while(right<s2.length()){
+            int[] check=new int[26];
+            for (int left=right-(s1.length()-1);left<=right;left++){
+                check[s2.charAt(left)-'a']++;
+            }
+            if(matches(bank,check)){
+                return true;
+            }
+
+            right++;
+        }
+        return false;
+        
+    }
+}
